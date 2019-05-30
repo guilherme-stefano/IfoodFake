a9f712b2a435d1abaadf0f01d04260b340427a4d
Guilherme Stéfano <guilherme.stefano@dartdigital.com.br>
2019-05-30 18:42:51 -0300

    adicionada conexão com o banco

--- NoFood_Api/bin/configuration/variables.js
+++ NoFood_Api/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables ={
     Api:{
         port: process.env.port || 3000
+    },
+    Database:{
+        connection:process.env.connection || 'mongodb+srv://root:root@cluster0-kjocf.mongodb.net/test?retryWrites=true&w=majority'
     }
 }
 
--- NoFood_Api/bin/express.js
+++ NoFood_Api/bin/express.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 //routers
 const categoriaRouter= require('../bin/routes/categoria-router');
@@ -10,7 +12,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//configurando a conexão com o banco de dados
 
+mongoose.connect(variables.Database.connection);
 //configurando rotas 
 app.use('/api/categoria', categoriaRouter);
 app.use('/api/produto', produtoRouter);
